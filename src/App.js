79c3d02777ae8a14f60e0e3668fe4d36c576fc5b
narcomarch <kramoogle@gmail.com>
2022-06-10 10:22:53 +0200

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bienvenue dans le front
+          Je fais un test de push
         </p>
         <a
           className="App-link"
